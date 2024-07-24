8197954a5cb17ca464fae408c11f1f0dabc7e2a2
doko972 david <doko972@gmail.com>
2024-07-24 15:17:58 +0200

    install npm sass

--- app/resources/views/page.blade.php
+++ app/resources/views/page.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    @vite('resources/css/styles.css')
+    @vite('resources/sass/app.scss')
 </head>
 
 <body>
--- app/vite.config.js
+++ app/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/styles.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
